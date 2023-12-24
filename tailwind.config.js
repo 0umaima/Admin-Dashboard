d2edfb843afa08d4084a37793ad631f7fc3430af
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-24 01:10:16 +0100

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],  theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-gray": "#F1F3F9",
+        "light-gray-1": "#787486",
+        "light-gray-2": "#8B8D97",
+        "light-gray-3": "#BEC0CA",
+        "dark-gray": "#45464E",
+        "light-pink": "#D235651A",
+        "light-pink-1": "#E01461",
+        "raspberry" : "#D23565"
+      },
+    },
   },
   plugins: [],
 }
