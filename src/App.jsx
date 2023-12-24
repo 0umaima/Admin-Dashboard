a0a0ce9f75963ad50b9f1743e3ee97a37be8a7d5
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-24 20:24:40 +0100

    add components routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import Layout from "./Layout/Layout"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import AddProduct from "./pages/AddProduct";
 
 function App() {
-
   return (
-    <Layout>
-    </Layout>
-  )
+    <>
+      <Router>
+        <Routes>
+         
+          <Route path="/" element={<Layout> <Home/> </Layout>} />
+          <Route path="/dashboard" element={<Layout> <Dashboard/> </Layout>} />
+          <Route path="/add-product" element={<Layout> <AddProduct/> </Layout>} />
+          <Route path="/profile" element={<Layout> <Profile/> </Layout>} />
+
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
