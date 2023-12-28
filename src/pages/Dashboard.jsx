7e31af3cb8512b8e69cb009e0c7c0017bd06350c
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-28 22:08:49 +0100

    update dashboard style

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ import Summary from "../components/dashboard/Summary";
 
 function Dashboard() {
   return (
-    <div className="grid grid-cols-1 grid-rows-8 md:grid-rows-20 lg:grid-rows-5 md:grid-cols-4 lg:grid-cols-7 gap-5 h-full">
+    <div className="grid grid-cols-1 grid-rows-8 md:grid-rows-10 lg:grid-rows-5 md:grid-cols-4 lg:grid-cols-7 gap-5 ">
       <OrdersList/>
       <Products/>
       <Cart/>
