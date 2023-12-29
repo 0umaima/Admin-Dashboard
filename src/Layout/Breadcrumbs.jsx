230fa2353ea38aee40b4837323cb1fcaeca9879a
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-29 01:01:57 +0100

    update component style

--- src/Layout/Breadcrumbs.jsx
+++ src/Layout/Breadcrumbs.jsx
@@ -6,16 +6,16 @@ function Breadcrumbs() {
     const pathnames = location.pathname.split('/').filter((pathname) => pathname !== '');
   
     return (
-      <div className="h-11 border-b-2 border-light-gray px-2 md:px-5 lg:px-7 flex items-center gap-5 font-DMmono">
+      <div className="h-11 border-b-2 border-light-gray px-2 md:px-5 lg:px-7 flex items-center gap-3 md:gap-5 font-DMmono">
         <Link to="/home">
-          <img src={home} alt="home" className="size-6"/>
+          <img src={home} alt="home" className="size-4 md:size-5"/>
         </Link>
         {pathnames.map((pathname, index) => {
           const routePath = `/${pathnames.slice(0, index + 1).join('/')}`;
           const isLast = index === pathnames.length - 1;
   
           return (
-            <div key={routePath} className="text-xl font-light flex justify-center gap-5 text-light-gray-2">
+            <div key={routePath} className="text-base lg:text-lg font-light flex justify-center gap-3 md:gap-5 text-light-gray-2">
               <span>/</span>
               {isLast ? (
                 <span>{pathname}</span>
