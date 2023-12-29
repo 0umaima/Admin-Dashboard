d07b731e5fd8372ccfbf094f9105aed2a472ce14
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-29 19:42:23 +0100

    update  gap style

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -11,7 +11,7 @@ import Summary from "../components/dashboard/Summary";
 function Dashboard() {
   return (
     <Layout>
-      <div className="grid grid-cols-1 grid-rows-11 md:grid-rows-10 lg:grid-rows-5 md:grid-cols-4 lg:grid-cols-7 gap-8 md:gap-5 ">
+      <div className="grid grid-cols-1 grid-rows-11 md:grid-rows-10 lg:grid-rows-5 md:grid-cols-4 lg:grid-cols-7 gap-7 md:gap-5 ">
         <OrdersList />
         <Products />
         <Cart />
