35e5e07d5136d24da5f744566d0d98937dc94db7
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-29 12:15:16 +0100

    add flowbite-react config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],  theme: {
     extend: {
       fontFamily: {
@@ -29,6 +30,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
