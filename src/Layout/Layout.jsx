6f75349df8c882a4aa3a72979e6ccc500b3b690d
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-29 12:22:04 +0100

    add Footer component to Layout

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import Breadcrumbs from "./Breadcrumbs";
 import Navbar from "./Navbar";
+import EndFooter from "./EndFooter"
 function Layout({ children }) {
   return (
     <>
       <Navbar />
       <Breadcrumbs />
       <div className="bg-light-gray p-2 md:p-5 lg:p-7">{children}</div>
+      <EndFooter/>
     </>
   );
 }
