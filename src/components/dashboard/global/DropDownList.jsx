fcc6370b71f7aacaa469a2dbfc135ba3b1658f0b
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-12-30 14:58:45 +0100

    optimize component

--- src/components/dashboard/global/DropDownList.jsx
+++ src/components/dashboard/global/DropDownList.jsx
@@ -2,42 +2,45 @@
 import { Dropdown } from "flowbite-react";
 //import hooks
 import { useListOptions } from "../../../hooks/useListOptions";
-//import icons
-import arrow from "../../../assets/icons/fi_chevron-down.svg";
 
-
-
-function DropDownList() {
-    const { listChoice, renderChoice } =
-    useListOptions();
+function DropDownList({choices, className, strokeColor}) {
+  const { listChoice, renderChoice } = useListOptions(choices);
   return (
     <>
-         <Dropdown
-          label=""
-          dismissOnClick={false}
-          renderTrigger={() => (
-            <div className="flex items-center gap-3">
-              <span className="text-light-gray-3 font-light font-Inter text-lg md:text-base">
-                {listChoice}
-              </span>
-              <button>
-                <img
-                  src={arrow}
-                  alt="arrow-down"
-                  className="size-6 md:size-5"
+      <Dropdown
+        label=""
+        dismissOnClick={false}
+        renderTrigger={() => (
+          <div className="flex items-center gap-3">
+            <span className={`flex items-center gap-3 ${className}`}>
+              {listChoice}
+            </span>
+            <button>
+            <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4 6L8 10L12 6"
+                  stroke={strokeColor || "#BEC0CA"}
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
-              </button>
-            </div>
-            
-          )}
-        >
-          <Dropdown.Item className=" font-inter text-light-gray-3 hover:text-light-gray-1 ">{renderChoice("This Week")}</Dropdown.Item>
-          <Dropdown.Item className=" font-inter text-light-gray-3 hover:text-light-gray-1 ">{renderChoice("This Month")}</Dropdown.Item>
-          <Dropdown.Item className=" font-inter text-light-gray-3 hover:text-light-gray-1 ">{renderChoice("This Year")}</Dropdown.Item>
-        </Dropdown>
+              </svg>
+            </button>
+          </div>
+        )}
+      >
+        {choices.map((choice) => (
+          <Dropdown.Item key={choice} className={className}>{renderChoice(choice)}</Dropdown.Item>
+        ))}
+      </Dropdown>
     </>
-   
-  )
+  );
 }
 
-export default DropDownList
+export default DropDownList;
