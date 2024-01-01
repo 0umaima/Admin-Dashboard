30497538bbfed7462364c08b8f7c1851a416c1bc
Aka-emmaa <Oumaimakhajaa@gmail.com>
2024-01-01 20:11:12 +0100

    get sales data from db

--- src/components/dashboard/Sales.jsx
+++ src/components/dashboard/Sales.jsx
@@ -1,11 +1,25 @@
+import { useState, useEffect } from "react";
 //import components
-import DropDownList from "./global/DropDownList"
+import DropDownList from "./global/DropDownList";
 //import icons
 import sales from "../../assets/icons/sales.svg";
-
+//import db 
+import salesDb from "../../../db.json"
 
 function Sales() {
-  const choices = ['Last Week', 'Last Month', 'Last Year'];
+  const choices = ["Last Week", "Last Month", "Last Year"];
+
+  const [salesAmount, setSalesAmount] = useState(0);
+  const [salesPercentage, setSalesPercentage] = useState(0);
+  const [volume, setVolume] = useState(0);
+
+  
+  useEffect(() => {
+    // Set the initial state using the data from db.json
+    setSalesAmount(salesDb.sales.amount);
+    setSalesPercentage(salesDb.sales.percentage);
+    setVolume(salesDb.sales.volume);
+  }, []);
 
   return (
     <div
@@ -16,7 +30,7 @@ function Sales() {
         <div className="bg-light-pink size-12 md:size-10 rounded-lg flex items-center justify-center">
           <img src={sales} alt="Sales" className="size-7 md:size-6" />
         </div>
-        <DropDownList choices={choices} className="text-light-gray-3"/>
+        <DropDownList choices={choices} className="text-light-gray-3" />
       </div>
       <div className="flex items-center gap-14 md:gap-16 lg:gap-11 xl:gap-16">
         <div className="flex flex-col justify-start  gap-3">
@@ -25,10 +39,11 @@ function Sales() {
           </span>
           <div className="flex items-center gap-2">
             <span className="text-2xl md:text-xl xl:text-2xl font-medium text-dark-gray font-Poppins">
-              ₦0.00
+            ₦{salesAmount}K
             </span>
             <span className="text-sm md:text-xs font-normal text-green-600 font-Inter">
-              +0.00%
+              {salesPercentage >= 0 ? "+" : "-"}
+              {Math.abs(salesPercentage.toFixed(2))}%
             </span>
           </div>
         </div>
@@ -37,7 +52,7 @@ function Sales() {
             Volume
           </span>
           <span className="text-2xl md:text-xl xl:text-2xl font-medium text-dark-gray font-Poppins">
-            0
+            {volume}
           </span>
         </div>
       </div>
